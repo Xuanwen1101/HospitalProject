cd4756f63a3cd3f65c7ced1fe178a6ab6baa50d4
d_219 <d_219@DESKTOP-Q7SP3HB>
2022-07-13 11:09:53 -0400

    Adding and Update file List, Details

---
+++ HospitalProject-Group3/Models/ViewModels/DetailsMedication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProject_Group3.Models.ViewModels
+{
+    public class DetailsMedication
+    {
+        public MedicationDto SelectedMedication { get; set; }
+        //all of staffs that worked as the selected Role
+        //public IEnumerable<PatientDto> WorkedStaffs { get; set; }
+    }
+}
