c1842dc5e5c70fc2c62c95876dd74a9ea9ccdef3
d_219 <d_219@DESKTOP-Q7SP3HB>
2022-07-13 10:03:37 -0400

    Adding A New File

---
+++ HospitalProject-Group3/Controllers/MedicationController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Diagnostics;
+using HospitalProject_Group3.Models;
+using HospitalProject_Group3.Models.ViewModels;
+using System.Web.Script.Serialization;
+
+namespace HospitalProject_Group3.Controllers
+{
+    public class MedicationController : Controller
+    {
+        private static readonly HttpClient client;
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+
+        static MedicationController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false,
+                //cookies are manually set in RequestHeader
+                UseCookies = false
+            };
+
+            client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:44342/api/");
+        }
+
+        /// <summary>
+        /// Grabs the authentication cookie sent to this controller.
+        /// For proper WebAPI authentication, you can send a post request with login credentials to the WebAPI and log the access token from the response. The controller already knows this token, so we're just passing it up the chain.
+        /// 
+        /// Here is a descriptive article which walks through the process of setting up authorization/authentication directly.
+        /// https://docs.microsoft.com/en-us/aspnet/web-api/overview/security/individual-accounts-in-web-api
+        /// </summary>
+        private void GetApplicationCookie()
+        {
+            string token = "";
+
+            client.DefaultRequestHeaders.Remove("Cookie");
+            if (!User.Identity.IsAuthenticated) return;
+
+            HttpCookie cookie = System.Web.HttpContext.Current.Request.Cookies.Get(".AspNet.ApplicationCookie");
+            if (cookie != null) token = cookie.Value;
+
+            //collect token as it is submitted to the controller
+            //use it to pass along to the WebAPI.
+            Debug.WriteLine("Token Submitted is : " + token);
+            if (token != "") client.DefaultRequestHeaders.Add("Cookie", ".AspNet.ApplicationCookie=" + token);
+
+            return;
+        }
+
+        /// GET: Medication/List
+        public ActionResult List()
+        {
+            //objective: communicate with the data api to retrieve a list of Shifts
+            //curl https://localhost:44342/api/MedicationData/ListMedications
+
+            string url = "MedicationData/ListMedications";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            IEnumerable<MedicationDto> medications = response.Content.ReadAsAsync<IEnumerable<MedicationDto>>().Result;
+
+            return View(medications);
+        }
+
+        // GET: Medication/New
+        /*[Authorize]*/
+        public ActionResult New()
+        {
+            /*
+            string url = "MedicationData/ListMedications";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            IEnumerable<MedicationDto> Medication = response.Content.ReadAsAsync<IEnumerable<MedicationDto>>().Result;
+            */
+            return View();
+        }
+
+        // POST: Medication/Create
+        [HttpPost]
+        /*[Authorize]*/
+        public ActionResult Create(Medication medication)
+        {
+            GetApplicationCookie();//get token credentials
+            //objective: add a new medication into our system using the API
+            //curl -H "Content-Type:application/json" -d @medication.json https://localhost:44342/api/MedicationData/AddMedication
+            string url = "MedicationData/AddMedication";
+
+            string jsonPayload = jss.Serialize(medication);
+
+            Debug.WriteLine("the json payload is :", jsonPayload);
+
+            HttpContent content = new StringContent(jsonPayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+
+        // GET: Medication/DeleteConfirm/5
+        /*[Authorize]*/
+        public ActionResult DeleteConfirm(int id)
+        {
+            // get the existing role information
+            //curl https://localhost:44342/api/MedicationData/FindData/{id}
+            string url = "MedicationData/FindData" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            MedicationDto selectedMedication = response.Content.ReadAsAsync<MedicationDto>().Result;
+
+            return View(selectedMedication);
+
+        }
+
+        // POST: Medication/Delete/5
+        [HttpPost]
+        /*[Authorize]*/
+        public ActionResult Delete(int id)
+        {
+            GetApplicationCookie();//get token credentials
+            //objective: delete the selected role from our system using the API
+            //curl -d "" https://localhost:44342/api/MedicationData/DeleteMedication/{id}
+            string url = "MedicationData/DeleteMedication/" + id;
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+    }
+}
---
+++ HospitalProject-Group3/Controllers/MedicationDataController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HospitalProject_Group3.Models;
+using System.Diagnostics;
+
+namespace HospitalProject_Group3.Controllers
+{
+    public class MedicationDataController : ApiController
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        /// <summary>
+        /// Returns all Medication in the system.
+        /// </summary>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// CONTENT: all Medication in the database.
+        /// </returns>
+        /// <example>
+        /// GET: api/MedicationData/ListMedications
+        /// </example>
+        [HttpGet]
+        [ResponseType(typeof(MedicationDto))]
+        public IHttpActionResult ListMedications()
+        {
+            List<Medication> Medications = db.Medications.ToList();
+            List<MedicationDto> MedicationDtos = new List<MedicationDto>();
+
+            Medications.ForEach(s => MedicationDtos.Add(new MedicationDto()
+            {
+                MedicationID=s.MedicationID,
+                MedicationName=s.MedicationName,
+                MedicationBrand=s.MedicationBrand,
+                MedicationDetail=s.MedicationDetail,
+                Price=s.Price
+            }));
+
+
+            return Ok(MedicationDtos);
+
+        }
+
+        /// <summary>
+        /// Adds a Medication to the system
+        /// </summary>
+        /// <param name="Medication">JSON FORM DATA of a Medication</param>
+        /// <returns>
+        /// HEADER: 201 (Created)
+        /// CONTENT: Medication ID, Medication Data
+        /// or
+        /// HEADER: 400 (Bad Request)
+        /// </returns>
+        /// <example>
+        /// POST: api/MedicationData/AddMedication
+        /// FORM DATA: Medication JSON Object
+        /// </example>
+        [ResponseType(typeof(Medication))]
+        [HttpPost]
+        /*[Authorize]*/
+        public IHttpActionResult AddMedication(Medication medication)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Medications.Add(medication);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = medication.MedicationID }, medication);
+        }
+
+        /// <summary>
+        /// Deletes an Role from the system by it's ID.
+        /// </summary>
+        /// <param name="id">The primary key of the Role</param>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// or
+        /// HEADER: 404 (NOT FOUND)
+        /// </returns>
+        /// <example>
+        /// DELETE: api/MedicationData/DeleteMedication/5
+        /// FORM DATA: (empty)
+        /// </example>
+        [ResponseType(typeof(Role))]
+        [HttpPost]
+        /*[Authorize]*/
+        public IHttpActionResult DeleteRole(int id)
+        {
+            Role role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            db.Roles.Remove(role);
+            db.SaveChanges();
+
+            return Ok(role);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RoleExists(int id)
+        {
+            return db.Roles.Count(e => e.RoleID == id) > 0;
+        }
+    }
+}
