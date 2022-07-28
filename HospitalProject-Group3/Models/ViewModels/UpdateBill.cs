b146ca09c191fd40c79a2eef9653e90ad96f358b
LAPTOP-56J4U8VG\lydia <xuanwen1101@outlook.com>
2022-07-28 00:33:12 -0400

    Fix issues on Patient, Appointment, and Bill

---
+++ HospitalProject-Group3/Models/ViewModels/UpdateBill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProject_Group3.Models.ViewModels
+{
+    public class UpdateBill
+    {
+        public BillDto SelectedBill { get; set; }
+
+        public IEnumerable<PatientDto> PatientOptions { get; set; }
+    }
+}
