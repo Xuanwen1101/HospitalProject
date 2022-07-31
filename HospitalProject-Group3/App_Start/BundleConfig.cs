31017eff0d25d931112656087240215c05c8e870
LAPTOP-56J4U8VG\lydia <xuanwen1101@outlook.com>
2022-07-31 18:11:45 -0400

    Edit Home page style

--- HospitalProject-Group3/App_Start/BundleConfig.cs
+++ HospitalProject-Group3/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace HospitalProject_Group3
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
