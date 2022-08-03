1fd306e8aca29155f80fc73aedea625c3c9f0fe9
Rohit Sharma <pc@DESKTOP-H94GUCO>
2022-08-03 09:25:41 -0400

    medicalcontroller5edit

---
+++ HospitalProject-Group3/Controllers/MedicalRecordsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalProject_Group3.Controllers
+{
+    public class MedicalRecordController : Controller
+    {
+        // GET: MedicalRecord
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
